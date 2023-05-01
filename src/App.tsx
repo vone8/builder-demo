aaa58dead21cbaa3fe2762492adbc280286b98bf
Vijay <vijay.bapodara@bytestechnolab.com>
2023-05-01 16:55:51 +0530

    App Tsx Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from "react";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+
+// Put your API key here
+builder.init("920a9a8d87c647f7a895f824b2812f5e");
+
+// set whether you're using the Visual Editor,
+// whether there are changes,
+// and render the content if found
+export default function CatchAllRoute() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent]:any = useState(null);
+
+  // get the page content from Builder
+  useEffect(() => {
+    async function fetchContent() {
+      const content = await builder
+        .get("page", {
+          url: window.location.pathname
+        })
+        .promise();
+
+      setContent(content);
+      setNotFound(!content);
+
+      // if the page title is found, 
+      // set the document title
+      if (content?.data.title) {
+        document.title = content.data.title
+      }
+    }
+    fetchContent();
+  }, [window.location.pathname]);
+
+  // If no page is found, return 
+  // a 404 page from your code.
+  // The following hypothetical 
+  // <FourOhFour> is placeholder.
+  if (notFound && !isPreviewingInBuilder) {
+    return <>Page Not Found</>
+  }
+
+  // return the page when found
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={content} />
+    </>
   );
 }
-
-export default App;
